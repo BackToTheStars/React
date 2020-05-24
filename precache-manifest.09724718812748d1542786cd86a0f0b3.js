ebda2bcfd19245c554f8c5de6f9121d25abf57e3
BackToTheStars <nikgrigoryev@gmail.com>
2020-05-23 20:43:49 -0700

    Updates

---
+++ precache-manifest.09724718812748d1542786cd86a0f0b3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e64a09a3115170da0afaeff911ad0797",
+    "url": "/React/index.html"
+  },
+  {
+    "revision": "1e0fc04c7a7dadea5816",
+    "url": "/React/static/css/main.1e79d560.chunk.css"
+  },
+  {
+    "revision": "5f0dfa8e443fb1b97ac1",
+    "url": "/React/static/js/2.33cd2b55.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React/static/js/2.33cd2b55.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e0fc04c7a7dadea5816",
+    "url": "/React/static/js/main.10254def.chunk.js"
+  },
+  {
+    "revision": "272f49613bcbca63689f",
+    "url": "/React/static/js/runtime-main.1088e224.js"
+  }
+]);
